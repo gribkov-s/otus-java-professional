0c0177e1cc8a33dd5a721a74b34648e121a69084
Сергей Грибков <sgribkov@beeline.ru>
2023-10-07 21:21:31 +0300

    hw10

---
+++ hw10/src/main/java/ru/otus/crm/model/Phone.java
@@ -0,0 +1,42 @@
+package ru.otus.crm.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "phone")
+
+public class Phone implements Cloneable {
+
+    @Id
+    @SequenceGenerator(name = "phone_gen", sequenceName = "phone_seq", initialValue = 1, allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "phone_gen")
+    @Column(name = "id", nullable = false, unique = true)
+    private Long id;
+
+    @Column(name = "number")
+    private String number;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "client_id")
+    private Client client;
+
+    public Phone(String number) {
+        this.id = null;
+        this.number = number;
+    }
+
+    public Phone(Long id, String number) {
+        this.id = id;
+        this.number = number;
+    }
+
+    @Override
+    @SuppressWarnings({"java:S2975", "java:S1182"})
+    public Phone clone() {
+        return new Phone(this.getId(), this.getNumber());
+    }
+}
